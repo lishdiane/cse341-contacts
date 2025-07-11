314de738fb25bfb277603c234319bfa91f397d8c
lishdiane <dlish@byupathway.edu>
2025-07-11 17:56:06 -0600

    swagger change host

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: "My API",
     description: "Contacts API",
   },
-  host: "localhost:3000",
-  schemes: ["http"],
+  host: "https://cse341-contacts-j9st.onrender.com",
+  schemes: ["https"],
 };
 
 const outputFile = "./swagger.json";
